14012cc8f1212fa8606eee0c6e61f469397c88f7
Woodward.Digital <82341943+woodward-digital@users.noreply.github.com>
2021-04-16 21:04:42 +0100

    Add files via upload
    Initial upload - Version 1.0

---
+++ The Network Monitor/Run-TheNetworkMonitor.ps1
@@ -0,0 +1,47 @@
+﻿###########################################################################################
+# Script Name: The Network Monitor
+# Script Purpose: The Network Monitor - Monitor Devices by pinging them
+# Created By: Woodward.Digital
+# Website: https://woodward.digital
+# Email: contact@woodward.digital
+# Version: v1.0
+###########################################################################################
+# Set Loop
+$Exit = "False"
+Do{
+# Reset Counter
+$AvailableDevices = 0
+$UnavailableDevices = 0
+
+Write-Host "The Network Monitor -"(Get-Date -format "dd-MMM-yyyy HH:mm:ss")
+
+# CSV File Import
+$DeviceList = Import-Csv -Path "G:\Michael\OneDrive\Desktop\The Network Monitor\DeviceList.csv"
+
+# Ping Devices
+Foreach ($Device in $DeviceList){
+    $DeviceName = $Device.Name
+    $DeviceAddr = $Device.IP
+
+if(Test-Connection -ComputerName $DeviceAddr -Quiet -ErrorAction SilentlyContinue -Count 1){
+    # Device is available
+    $AvailableDevices ++
+    }
+Else{
+    # Device is not available
+    Write-Host "Unavailable host -----> "$DeviceName" " -BackgroundColor Red -ForegroundColor White
+    $UnavailableDevices ++
+    }
+}
+
+# Output results
+write-host "-----------------------------------------------------"
+write-host "Monitoring summary"
+Write-Host "Available device count:"$AvailableDevices
+Write-Host "Unavailable device count:"$UnavailableDevices
+write-host "-----------------------------------------------------"
+
+# Time between re-tests
+Timeout /t 60
+Clear-Host
+}While ($Exit -ne $True)
